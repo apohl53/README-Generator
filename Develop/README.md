12832b309757f6a460ba37115251a04f89574e96
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-09-30 16:28:54 -0400

    fixed issues with questions not matching markdown function. Successfully created README using node

---
+++ Develop/README.md
@@ -0,0 +1,30 @@
+# README Generator
+
+## Description
+This project helps you quickly create a README file for any project!
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+The user should clone this repo from Github and download Node. This application also requires a file system and inquirer 
+
+## Usage
+Use inquirer from the command line to answer the questions about your project.
+
+## License
+This project is licensed under MIT
+
+## Contributing 
+Users should read the installation section and make sure they have the necessary files.
+
+## Tests
+node index.js
+
+## Questions
+If you have any questions about this projects, please contact me directly at apohlman53@gmail.com. You can view more of my projects at https://github.com/Apohl53.
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,52 +1,35 @@
-// A function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  switch (license) {
-    case "MIT":
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    case "Apache 2.0":
-      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    case "GNU GPLv3":
-      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-    default:
-      return "";
-  }
-}
-
-// A function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  switch (license) {
-    case "MIT":
-      return "[MIT](https://opensource.org/licenses/MIT)";
-    case "Apache 2.0":
-      return "[Apache 2.0](https://opensource.org/licenses/Apache-2.0)";
-    case "GNU GPLv3":
-      return "[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)";
-    default:
-      return "";
-  }
-}
-
-// A function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license) {
-    return `## License
-
-This project is licensed under the ${license} license. Click [here](${renderLicenseLink(
-      license
-    )}) for more information.`;
-  } else {
-    return "";
-  }
-}
-
 // A function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-  ${renderLicenseSection(data.license)}
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under ${data.license}
+
+## Contributing 
+${data.contributors}
+
+## Tests
+${data.test}
+
+## Questions
+If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
 `;
 }
 
